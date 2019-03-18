4b78064411c125dae3f21452e5cb2dc0b006e46c
AhmedMAbdo <windwave41@yahoo.com>
2019-03-18 12:46:52 +0200

    Adding Search Bar with one error found

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Card = ({name, email , id}) => {
     
     // const Card = (props) => {
@@ -7,14 +8,15 @@ const Card = ({name, email , id}) => {
 return (
 
 <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-    <img src={`https://robohash.org/${id}?200x200`} alt='Janes Doe' />
+    <img src={`https://robohash.org/${id}?200x200`} alt='Robot Based On Char.' />
     <div>
-        <h2>J{name}</h2>
+        <h2>{name}</h2>
         <p>{email}</p>
     </div>
 
 
 </div>
+
 
 
 );
