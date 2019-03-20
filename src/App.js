5cce0ff717c370f88d71ff9459a139e1c4469ebc
AhmedMAbdo <windwave41@yahoo.com>
2019-03-20 14:16:00 +0200

    Adding Stylings to Font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from "./SearchBox";
+import './App.css';
 
 
 class App extends Component {
     constructor() {
-        super()
+        super();
         this.state = { 
             robots: robots,
             searchfield: ''
@@ -19,15 +20,15 @@ class App extends Component {
        
       
 
-    }
+    };
     render() {
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
+        });
 
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1> 
+                <h1 className='f1'>RoboFriends</h1> 
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
     
